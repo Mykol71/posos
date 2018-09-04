9f047bfa3e47d7c1d052036a2a3b2d843f377483
Mike Green <mgreen@mykol.com>
2018-09-03 21:02:19 -0500

    2018-09-03 21:02

--- format_code.sh
+++
@@ -1,5 +0,0 @@
-for FILE in "`find . -name "*_*" | grep -v ostools`"; do
-shfmt $FILE
-#echo "mv -f "$FILE_new" "$FILE""
-#mv -f $FILE_new $FILE
-done
--- readme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-clear
-cat ./bin/*_* | grep "^#" | grep -v bash | sed -e 's/^#//g'
