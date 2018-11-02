5d7df81ca88905a56e10f379725f5e55edd99220
Mike Green <mgreen@mykol.com>
2018-11-02 03:04:43 +0000

    2018-11-01 22:12 - Menu Cosmetic

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+printf '\u250c'
+printf '\u2500'
+printf '\u2500'
+printf '\u2500'
+printf '\u2500'
+printf '\u2500'
+printf '\u2500'
+printf '\u2500'
+printf '\u2500'
+printf '\u2500'
+printf '\n\n'
